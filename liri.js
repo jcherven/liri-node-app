ed26c1e053e5ebf7fd9ad90e15e696c46aabfa32
James Cherven <jcherven@mail.usf.edu>
2018-10-23 22:55:19 -0400

    Adds input handling and usage help on the command line

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+
 const keys = require('./keys.js');
 var request = require('request');
 var Spotify = require('node-spotify-api'); 
@@ -6,6 +7,17 @@ var spotify = new Spotify(keys.spotify);
 
 var option = process.argv[2];
 var argument = process.argv[3];
+
+var helpText = "LIRI USAGE HELP: \n\n\
+    $ node liri <options> <argument>\n\n\
+    Options:\n\
+    \tconcert-this\n\
+    \tspotify-this-song\n\
+    \tmovie-this\n\
+    \tdo-what-it-says\n\n\
+    <argument> must be quoted (i.e., \"Ace of Base\")\n"
+var optHelpText = "<option requires an argument (i.e., $ liri concert-this \"Ace of Base\""
+var argHelpText = "<argument> must be quoted (e.g., \"Ace of Base\", \"Eraserhead\", \"Everybody (Backstreet's Back)\")"
 
 switch ( option ) {
     case 'concert-this':
@@ -24,28 +36,59 @@ switch ( option ) {
         doWhatItSays(argument);
         break;
 
+    default:
+        inputHelp();
+        break;
+
     case 'help':
-        displayHelp();
+        inputHelp();
         break;
 }; // End switch
 
 function concertThis(artist) {
-    console.log( 'concert-this passed with argument ' + artist);
+    if ( typeof argument === 'undefined' ) {
+        console.log(argHelpText);
+        return;
+    }
 
+    console.log( 'concert-this passed with argument ' + artist);
 }
 
 function spotifyThis(artist) {
-    console.log( 'spotify-this-song passed with argument ' + artist );
+    if ( typeof argument === "undefined" ) {
+        console.log(argHelpText);
+        return;
+    }
 
+    console.log( 'spotify-this-song passed with argument ' + artist );
 }
 
 function movieThis(movieTitle) {
-    console.log( 'concert-this passed with argument ' + movieTitle );
+    if ( typeof argument === "undefined" ) {
+        console.log(argHelpText);
+        return;
+    }
 
+    console.log( 'concert-this passed with argument ' + movieTitle );
 }
 
 function doWhatItSays(whatItSays) {
+    if ( typeof argument === 'undefined' ) {
+        console.log(argHelpText);
+        return;
+    }
+
     console.log( 'do-what-it-says passed with argument ' + whatItSays );
-    
+}
+
+function inputHelp(argument) {
+    if ( !option && !argument )
+        console.log(helpText);
+    else if ( option != 'help' && !argument )
+        console.log(optHelpText);
+    else if ( option != 'help' && typeof argument === 'undefined' )
+        console.log(argHelpText)
+    else
+        console.log(helpText);
 }
 
