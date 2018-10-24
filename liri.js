99662b4eed3106a2caec4d8c0f7c70098acb18ef
James Cherven <jcherven@mail.usf.edu>
2018-10-24 11:07:05 -0400

    Adds concert-this feature

--- liri.js
+++ liri.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
+
 const keys = require('./keys.js');
-var request = require('request');
+var colors = require('colors');
+var request = require('request-json');
+var client = request.createClient('http://localhost:8888/');
 var Spotify = require('node-spotify-api'); 
 var spotify = new Spotify(keys.spotify);
 
@@ -25,7 +28,6 @@ var helpText = "LIRI USAGE HELP: \n\n\
     \tdo-what-it-says\n\n\
     <argument> must be quoted for predictable behavior (i.e., \"Ace of Base\")\n";
 
-var optHelpText = "<option requires an argument (i.e., $ liri concert-this \"Ace of Base\"";
 var argHelpText = "Quoted argument required with option (e.g., \"Ace of Base\", \"Eraserhead\", \"Everybody (Backstreet's Back)\")";
 
 switch ( option ) {
@@ -55,7 +57,33 @@ switch ( option ) {
 }; // End switch
 
 function concertThis(artist) {
-    console.log( 'concert-this passed with argument ' + artist);
+    var bandsApiErrorText = "There was a little problem getting your concert info.\nPlease try again later, or try a different artist."
+    artist = artist.split(' ').join('+');
+    var bandsApiQuery = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    client.get(bandsApiQuery, function(error, response, body) {
+        if ( typeof body === "undefined" || error || response.statusCode != 200 ) {
+            console.log(bandsApiErrorText);
+        }
+        var isLocal = true;
+        var artistName = body[0].lineup[0];
+        var venueLocal = body[0].venue.name + " in " + body[0].venue.city + ', ' + body[0].venue.region;
+        var venueIntl = body[0].venue.name + " in " + body[0].venue.city + ', ' + body[0].venue.country;
+        var date = body[0].datetime;
+
+        // Check if the show is local or international
+        if ( body[0].venue.region === "" )
+            isLocal = false;
+
+        // Render the name of the venue, location, and date of the event in MM/DD/YYYY
+        console.log("\nThe next " + artistName.cyan.bold +  " show's info is:");
+        console.log("On: " + date.yellow.bold);
+        if ( isLocal )
+            console.log("At: " + venueLocal.magenta.bold + "\n");
+        else
+            console.log("At: " + venueIntl.magenta.bold + "\n");
+    })
+
 };
 
 function spotifyThis(artist) {
@@ -73,8 +101,6 @@ function doWhatItSays(whatItSays) {
 function inputHelp(arg) {
     if ( !optsList.includes(arg) )
         console.log(helpText);
-    
     else if ( optsList.includes(arg) && !arg )
         console.log(argHelpText);
 };
-
